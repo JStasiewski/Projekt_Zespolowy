23039ae7480eb4ec2559147dcbf895d151616e27
Piotr <piotr9833@gmail.com>
2023-05-23 16:25:07 +0200

    Stworzono element który ma byc odpowiedzialny za jakieś info z konta nwm jeszcze jakie info

---
+++ group-project/src/views/AccInfoComponents/MainAccInfo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<script lang="ts">
+  export default {
+    props:{
+      ApiKey: String,
+    },
+    data(){
+      return{
+      };
+    },
+    methods:{
+    }
+  }
+</script>
+
+<style>
+</style>
